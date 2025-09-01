4974f70787e8eca1ae31f656d11736a55cc97a7b
rrain <rraines22@proton.me>
2025-09-01 16:13:08 -0400

    adding more tests

--- src/test/java/com/rainesinc/dag/SearchTest.java
+++ src/test/java/com/rainesinc/dag/SearchTest.java
@@ -9,8 +9,13 @@ import java.util.*;
 public class SearchTest {
 
     @Test
-    public void testLongestPathFromNode2(){
+    public void testLongestPathFromNode2CaseMistakenlyOverwriteContentsOfGraph(){
         Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        graph.put(0, Arrays.asList(1, 3));
+        graph.put(2, Arrays.asList(4));
+        graph.put(4, Arrays.asList(3));
+
         graph.put(0, Arrays.asList(1, 3));
         graph.put(2, Arrays.asList(4));
         graph.put(4, Arrays.asList(3));
@@ -18,6 +23,36 @@ public class SearchTest {
         Map<Integer, Integer> store = new HashMap<>();
 
         int longestPathFromStart = Search.depthFirstSearch(2, graph, store);
+        Assert.assertEquals(2, longestPathFromStart);
+    }
+
+    @Test
+    public void testLongestPathFromNode2CaseMistakenlyOverwriteNodeNamesInGraph(){
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        graph.put(0, Arrays.asList(1, 3, 3));
+        graph.put(2, Arrays.asList(4));
+        graph.put(4, Arrays.asList(3));
+
+        Map<Integer, Integer> store = new HashMap<>();
+
+        int longestPathFromStart = Search.depthFirstSearch(2, graph, store);
+        Assert.assertEquals(2, longestPathFromStart);
+    }
+
+    @Test
+    public void testLongestPathFromNode2CaseLargerGraph(){
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        graph.put(0, Arrays.asList(1, 3));
+        graph.put(2, Arrays.asList(4));
+        graph.put(4, Arrays.asList(3));
+        graph.put(5, Arrays.asList(8, 9, 11));
+        graph.put(11, Arrays.asList(44, 12, 15));
+
+        Map<Integer, Integer> store = new HashMap<>();
+
+        int longestPathFromStart = Search.depthFirstSearch(5, graph, store);
         Assert.assertEquals(2, longestPathFromStart);
     }
 
